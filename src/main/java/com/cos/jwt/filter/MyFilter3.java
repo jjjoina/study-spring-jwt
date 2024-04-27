1df6b109d1058f1b97d7815cbcd5ddcb8b5347b8
조인화 <ihnhwa8362@naver.com>
2024-04-27 21:21:21 +0900

    23강 - jwt 임시 토큰 만들어서 테스트 해보기

--- src/main/java/com/cos/jwt/filter/MyFilter3.java
+++ src/main/java/com/cos/jwt/filter/MyFilter3.java
@@ -1,12 +1,15 @@
 package com.cos.jwt.filter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class MyFilter3 implements Filter {
 
@@ -14,7 +17,22 @@ public class MyFilter3 implements Filter {
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws
 		IOException,
 		ServletException {
-		System.out.println("필터3");
-		filterChain.doFilter(servletRequest, servletResponse);
+		HttpServletRequest req = (HttpServletRequest) servletRequest;
+		HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+		// cos라는 토큰을 만들었다고 가정
+		if (req.getMethod().equals("POST")) {
+			System.out.println("POST 요청됨");
+			String headerAuth = req.getHeader("Authorization");
+			System.out.println("headerAuth = " + headerAuth);
+			System.out.println("필터3");
+
+			if (headerAuth.equals("cos")) {
+				filterChain.doFilter(req, res);
+			} else {
+				PrintWriter out = res.getWriter();
+				out.println("인증 안 됨");
+			}
+		}
 	}
 }
