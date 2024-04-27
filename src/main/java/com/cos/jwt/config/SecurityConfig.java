f370c2d4d971c93b8e7563987091fddab0daca78
조인화 <ihnhwa8362@naver.com>
2024-04-27 18:31:35 +0900

    20강 - jwt를 위한 security 설정

---
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.cos.jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final CorsFilter corsFilter;
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(CsrfConfigurer::disable);
+		http
+			.sessionManagement(sessionManagement ->
+				sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			)
+			.addFilter(corsFilter)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(authorize ->
+				authorize
+					.requestMatchers("/api/v1/user/**").hasAnyRole("USER", "MANAGER", "ADMIN")
+					.requestMatchers("/api/v1/manager/**").hasAnyRole("MANAGER", "ADMIN")
+					.requestMatchers("/api/v1/admin/**").hasAnyRole("ADMIN")
+					.anyRequest().permitAll()
+			);
+
+		return http.build();
+	}
+}
