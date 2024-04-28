90dbb8f9fb02dc298741d607f969cf528437cced
조인화 <ihnhwa8362@naver.com>
2024-04-28 16:19:14 +0900

    24강 - jwt를 위한 로그인 시도

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -2,16 +2,16 @@ package com.cos.jwt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
-import com.cos.jwt.filter.MyFilter3;
+import com.cos.jwt.config.jwt.JwtAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,7 +24,10 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class);
+
+		AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+
+		// http.addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class);
 		http.csrf(CsrfConfigurer::disable);
 		http
 			.sessionManagement(sessionManagement ->
@@ -33,6 +36,7 @@ public class SecurityConfig {
 			.addFilter(corsFilter)
 			.formLogin(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable)
+			.addFilter(new JwtAuthenticationFilter(authenticationManager))
 			.authorizeHttpRequests(authorize ->
 				authorize
 					.requestMatchers("/api/v1/user/**").hasAnyRole("USER", "MANAGER", "ADMIN")
